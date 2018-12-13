e4b88d15015372dc103f8724c7242563af5db100
Axel Demborg <axel.demborg@gmail.com>
2018-12-13 11:27:49 +0100

    working with embeddings?!

--- wordbasedLSTM.py
+++ wordbasedLSTM.py
@@ -52,13 +52,13 @@ from __future__ import print_function
 import string
 
 from keras.models import Model
-from keras.layers import Input, LSTM, Dense
+from keras.layers import Input, LSTM, Dense, Embedding
 import numpy as np
 
 batch_size = 64  # Batch size for training.
 epochs = 100  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
-num_samples = 1000  # Number of samples to train on.
+num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
 data_path = 'swe-eng/swe.txt'
 
@@ -130,19 +130,21 @@ for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
             decoder_target_data[i, t - 1, target_token_index[char]] = 1.
 
 # Define an input sequence and process it.
-encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder_inputs = Input(shape=(None, ))
+embedded_encoder_input = Embedding(num_encoder_tokens, 20)(encoder_inputs)
 encoder = LSTM(latent_dim, return_state=True)
-encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+encoder_outputs, state_h, state_c = encoder(embedded_encoder_input)
 # We discard `encoder_outputs` and only keep the states.
 encoder_states = [state_h, state_c]
 
 # Set up the decoder, using `encoder_states` as initial state.
-decoder_inputs = Input(shape=(None, num_decoder_tokens))
+decoder_inputs = Input(shape=(None, ))
+embedded_decoder_input = Embedding(num_decoder_tokens, 20)(decoder_inputs)
 # We set up our decoder to return full output sequences,
 # and to return internal states as well. We don't use the
 # return states in the training model, but we will use them in inference.
 decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
-decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+decoder_outputs, _, _ = decoder_lstm(embedded_decoder_input,
                                      initial_state=encoder_states)
 decoder_dense = Dense(num_decoder_tokens, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
@@ -153,7 +155,7 @@ model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
 # Run training
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+model.fit([encoder_input_data.argmax(-1), decoder_input_data.argmax(-1)], decoder_target_data,
           batch_size=batch_size,
           epochs=epochs,
           validation_split=0.2)
@@ -175,7 +177,7 @@ decoder_state_input_h = Input(shape=(latent_dim,))
 decoder_state_input_c = Input(shape=(latent_dim,))
 decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
 decoder_outputs, state_h, state_c = decoder_lstm(
-    decoder_inputs, initial_state=decoder_states_inputs)
+    embedded_decoder_input, initial_state=decoder_states_inputs)
 decoder_states = [state_h, state_c]
 decoder_outputs = decoder_dense(decoder_outputs)
 decoder_model = Model(
@@ -192,7 +194,7 @@ reverse_target_char_index = dict(
 
 def decode_sequence(input_seq):
     # Encode the input as state vectors.
-    states_value = encoder_model.predict(input_seq)
+    states_value = encoder_model.predict(input_seq.argmax(-1))
 
     # Generate empty target sequence of length 1.
     target_seq = np.zeros((1, 1, num_decoder_tokens))
@@ -205,7 +207,7 @@ def decode_sequence(input_seq):
     decoded_sentence = []
     while not stop_condition:
         output_tokens, h, c = decoder_model.predict(
-            [target_seq] + states_value)
+            [target_seq.argmax(-1)] + states_value)
 
         # Sample a token
         sampled_token_index = np.argmax(output_tokens[0, -1, :])
